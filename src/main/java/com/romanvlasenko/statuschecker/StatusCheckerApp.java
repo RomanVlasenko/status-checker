6e8ce7d57d67877a5bb4eb0b6ea28aa8c0a3a56d
ipcreeper <ipcreeper@gmail.com>
2014-03-30 19:21:19 +0300

    Changed approach of application start

--- src/main/java/com/romanvlasenko/statuschecker/StatusCheckerApp.java
+++ src/main/java/com/romanvlasenko/statuschecker/StatusCheckerApp.java
@@ -9,38 +9,37 @@ import java.util.Properties;
 
 public class StatusCheckerApp {
 
-  //To run app from IDE add absolute path to settings.conf as argument
-  public static void main(String... args) {
-    String confPath = "settings.conf";
+    public static void main(String... args) {
+        if (args.length > 0) {
+            String confPath = args[0];
+            Properties properties = new Properties();
+            try {
+                properties.load(new FileReader(new File(confPath)));
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Unable to load 'settings.conf'. Check provided path");
+            }
 
-    if (args.length > 0) {
-      confPath = args[0];
+            start(properties);
+        } else {
+            System.out.println("Path to 'settings.conf' is not specified");
+        }
     }
 
-    Properties properties = new Properties();
-    try {
-      properties.load(new FileReader(new File(confPath)));
-    } catch (IOException e) {
-      e.printStackTrace();
+    private static void start(Properties properties) {
+        HashMap<String, String> urlsToCheck = new HashMap<>();
+
+        for (Entry<Object, Object> urlEntry : properties.entrySet()) {
+            String key = (String) urlEntry.getKey();
+            String value = (String) urlEntry.getValue();
+
+            urlsToCheck.put(key, value);
+        }
+
+        TrayInstance trayInstance = new TrayInstance(urlsToCheck.keySet());
+
+        UrlChecker urlChecker = new UrlChecker(trayInstance, urlsToCheck);
+        urlChecker.start();
     }
-
-    start(properties);
-  }
-
-  private static void start(Properties properties) {
-    HashMap<String, String> urlsToCheck = new HashMap<>();
-
-    for (Entry<Object, Object> urlEntry : properties.entrySet()) {
-      String key = (String) urlEntry.getKey();
-      String value = (String) urlEntry.getValue();
-
-      urlsToCheck.put(key, value);
-    }
-
-    TrayInstance trayInstance = new TrayInstance(urlsToCheck.keySet());
-
-    UrlChecker urlChecker = new UrlChecker(trayInstance, urlsToCheck);
-    urlChecker.start();
-  }
 
 }
